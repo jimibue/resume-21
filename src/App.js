329fdb33b86db30169beaa9a8ebd475d1000401c
James Yeates <jimibue@gmail.com>
2021-10-11 10:54:00 -0600

    error fixed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import Card from "./components/Card";
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { x } from "./components/Dummy";
 
 var options = {
   method: "GET",
